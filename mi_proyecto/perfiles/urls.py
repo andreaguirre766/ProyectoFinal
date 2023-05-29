ee162307f2341ae8ffaeb484f1a16253730ec039
andreaguirre766 <andreaguirre766@gmail.com>
2023-05-28 21:33:56 -0300

    cambios

--- mi_proyecto/perfiles/urls.py
+++ mi_proyecto/perfiles/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from perfiles.views import registro , login_view
 from perfiles.views import registro, login_view, CustomLogoutView
+from perfiles.views import registro, login_view, CustomLogoutView, MiPerfilUpdateView
 
 
 
@@ -9,5 +10,6 @@ urlpatterns = [
      path("registro/",registro , name="registro"),
      path('login/', login_view, name="login"),
      path('logout/', CustomLogoutView.as_view(), name="logout"),
+     path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
     
      ]
