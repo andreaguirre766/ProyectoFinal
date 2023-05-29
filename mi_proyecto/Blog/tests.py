5ee694fced5de06f60bb344a666453b9172921ce
andreaguirre766 <andreaguirre766@gmail.com>
2023-05-29 01:48:40 -0300

    cambios

--- mi_proyecto/Blog/tests.py
+++ mi_proyecto/Blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class InfoMarcaTestCase(TestCase):
+    def test_info_marca_renderiza_template_correcto(self):
+        
+        url = reverse('info_marca')
+
+       
+        response = self.client.get(url)
+
+        
+        self.assertEqual(response.status_code, 200)
+
+       
+        self.assertTemplateUsed(response, 'Blog/infomarca.html')
+
+
---
+++ mi_proyecto/static/js/nike_blog.js
@@ -0,0 +1,20 @@
+// nike_blog.js
+
+// Este código se ejecuta cuando el DOM se ha cargado completamente
+document.addEventListener('DOMContentLoaded', function() {
+    // Agrega aquí tu código JavaScript personalizado
+    // Puedes utilizar los selectores de CSS para seleccionar elementos y agregar comportamiento
+  
+    // Ejemplo:
+    // Obtener el elemento con ID "miElemento"
+    var miElemento = document.getElementById('miElemento');
+  
+    // Agregar un evento de clic al elemento
+    miElemento.addEventListener('click', function() {
+      // Realizar alguna acción cuando se hace clic en el elemento
+      console.log('Se hizo clic en el elemento');
+    });
+  
+    // Puedes agregar más código JavaScript personalizado según tus necesidades
+  });
+  
